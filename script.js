ad2b6b5f5cd866f2ccc0b72a406e1eb99de1c5bb
Mark <138724906+mlenda000@users.noreply.github.com>
2023-08-02 11:10:22 -0400

    lalala

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// TODO: add code here
+
+window.addEventListener("load", function() {
+    
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+      response.json().then(function(json){
+    console.log(json);
+        for(let i=0;i< json.length; i++){
+            const destination = document.getElementById('container') 
+            let picture = json[i].picture
+            console.log(picture)
+            destination.innerHTML +=`
+            <div class="astronaut">
+                <div class="bio">
+                <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                <ul>
+                    <li>Hours in space: ${json[i].hoursInSpace}</li>
+                    <li>Active: ${json[i].active}</li>
+                    <li>Skills: ${json[i].skills}</li>
+                </ul>
+                </div>
+                <img class='avatar' src="${picture}">
+
+            </div>`
+        }
+
+      });
+    });
+  });
